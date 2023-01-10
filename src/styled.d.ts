9732449fd86024e58af58aca3db8153852308f55
jindonyy <rsefa94@gmail.com>
2023-01-11 01:57:55 +0900

    Chore: eslint, typescript css prop 설정

--- src/styled.d.ts
+++ src/styled.d.ts
@@ -3,3 +3,9 @@ import { Theme } from '@styles/theme';
 declare module 'styled-components' {
   export interface DefaultTheme extends Theme {}
 }
+
+declare module 'react' {
+  interface Attributes {
+    css?: CSSProp<DefaultTheme>;
+  }
+}
