bef3d37938721a09c9951446cc69713d3f75b037
jindonyy <rsefa94@gmail.com>
2023-01-11 01:56:52 +0900

    Chore: story book 절대경로 설정

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
@@ -9,5 +11,20 @@ module.exports = {
   framework: '@storybook/react',
   core: {
     builder: '@storybook/builder-webpack5'
+  },
+  webpackFinal: async config => {
+    config.resolve.modules = [path.resolve(__dirname, '..'), 'node_modules', 'styles'];
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@components': path.resolve(__dirname, '../src/components'),
+      '@pages': path.resolve(__dirname, '../src/pages'),
+      '@hooks': path.resolve(__dirname, '../src/hooks'),
+      '@styles': path.resolve(__dirname, '../src/styles'),
+      '@layout': path.resolve(__dirname, '../src/layout'),
+      '@apis': path.resolve(__dirname, '../src/apis'),
+      '@assets': path.resolve(__dirname, '../src/assets'),
+      '@utils': path.resolve(__dirname, '../src/utils')
+    };
+    return config;
   }
 };
