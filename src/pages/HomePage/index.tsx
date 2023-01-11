b3cc94efea69c99cd6bacf9f7d7ad65e5ed25b23
jindonyy <rsefa94@gmail.com>
2023-01-11 14:03:11 +0900

    Feat: HomePage 생성

---
+++ src/pages/HomePage/index.tsx
@@ -0,0 +1,10 @@
+import PageLayout from 'components/PageLayout';
+import $Title from 'components/Title';
+
+const HomePage = () => (
+  <PageLayout>
+    <$Title>Hueron Home Page</$Title>
+  </PageLayout>
+);
+
+export default HomePage;
