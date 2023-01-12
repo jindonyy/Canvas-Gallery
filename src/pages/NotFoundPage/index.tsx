f10d354366dd87ba2ab183504b324fa9dc120c6f
jindonyy <rsefa94@gmail.com>
2023-01-13 01:45:28 +0900

    Refactor: NotFound 페이지 Title에서 ErrorArea로 수정

--- src/pages/NotFoundPage/index.tsx
+++ src/pages/NotFoundPage/index.tsx
@@ -1,13 +1,10 @@
-import $Error from 'components/styled/Error/$ErrorArea';
-import $PageLayout from 'components/styled/PageLayout';
+import $ErrorArea from 'components/styled/Error/ErrorArea';
 
 const NotFound = () => (
-  <$PageLayout>
-    <$Error>
-      <$Error.Title>페이지를 찾을 수 없습니다.</$Error.Title>
-      <$Error.Description>경로를 확인해주세요.</$Error.Description>
-    </$Error>
-  </$PageLayout>
+  <$ErrorArea size="large">
+    <$ErrorArea.Title size="large">페이지를 찾을 수 없습니다.</$ErrorArea.Title>
+    <$ErrorArea.Description size="large">경로를 확인해주세요.</$ErrorArea.Description>
+  </$ErrorArea>
 );
 
 export default NotFound;
