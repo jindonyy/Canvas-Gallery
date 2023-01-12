5740b5b8ec87efa4c2cbe839aa0b7ed3f893b8f2
jindonyy <rsefa94@gmail.com>
2023-01-13 01:42:02 +0900

    Design: Title font-size 수정

--- src/components/styled/Title/index.tsx
+++ src/components/styled/Title/index.tsx
@@ -1,21 +1,27 @@
+import { ReactNode } from 'react';
 import styled from 'styled-components';
 
 type $TitleProps = {
   variant?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
+  children: ReactNode;
 };
 
 const TitleFontSizes = {
-  h1: '3.2rem',
-  h2: '2.8rem',
-  h3: '2.4rem',
-  h4: '2.2rem',
-  h5: '2rem',
-  h6: '1.8rem'
+  h1: '2.8rem',
+  h2: '2.4rem',
+  h3: '2.2rem',
+  h4: '2rem',
+  h5: '1.8rem',
+  h6: '1.6rem'
 };
 
-const $Title = styled.h1<$TitleProps>`
+const $TitleRoot = styled.h1<$TitleProps>`
   margin-bottom: 2rem;
   font-size: ${({ variant = 'h1' }) => TitleFontSizes[variant]};
 `;
+
+const $Title = ({ variant, children }: $TitleProps) => (
+  <$TitleRoot as={variant}>{children}</$TitleRoot>
+);
 
 export default $Title;
