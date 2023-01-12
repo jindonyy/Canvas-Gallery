355487c5b854ee39d04e2f5236641ecaf233418e
jindonyy <rsefa94@gmail.com>
2023-01-12 19:05:14 +0900

    Feat: Gallery 상세 페이지 추가

---
+++ src/pages/GalleryDetailPage/index.tsx
@@ -0,0 +1,9 @@
+import PageLayout from 'components/PageLayout';
+
+const GalleryDetailPage = () => (
+  <PageLayout>
+    <img src="https://picsum.photos/id/0/5000/3333" alt="제품 사진" />
+  </PageLayout>
+);
+
+export default GalleryDetailPage;
