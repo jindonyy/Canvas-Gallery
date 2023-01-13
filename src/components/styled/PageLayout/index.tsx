b18b8ff338182a559591a42d37d20eb672068bd5
jindonyy <rsefa94@gmail.com>
2023-01-13 20:18:15 +0900

    Design: PageLayout 위아래 여백 수정

--- src/components/styled/PageLayout/index.tsx
+++ src/components/styled/PageLayout/index.tsx
@@ -6,7 +6,7 @@ const $PageLayout = styled.div`
   min-width: ${({ theme }) => theme.size.media.min};
   min-height: calc(100vh - ${({ theme }) => theme.size.height.header});
   margin: 0 auto;
-  padding: 4rem 2rem;
+  padding: 3rem 2rem;
 `;
 
 export default $PageLayout;
