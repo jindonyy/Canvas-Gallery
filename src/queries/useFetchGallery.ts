8398ab2f39b83376afa0f9a8dfb75cba04fa8e05
jindonyy <rsefa94@gmail.com>
2023-01-16 03:01:57 +0900

    Feat: useFetchGallery에 query options prop 추가

--- src/queries/useFetchGallery.ts
+++ src/queries/useFetchGallery.ts
@@ -1,16 +1,19 @@
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, UseQueryOptions } from '@tanstack/react-query';
 import { AxiosError } from 'axios';
 
 import { fetchGallery, ImageEntity } from 'api/gallery';
 import { MINUTE } from 'utils/unit';
 
-const useFetchGallery = () =>
-  useQuery<ImageEntity[], AxiosError>(['gallery'], fetchGallery, {
+const useFetchGallery = (
+  options?: UseQueryOptions<ImageEntity[], AxiosError, ImageEntity[], ['gallery']>
+) =>
+  useQuery(['gallery'], fetchGallery, {
     staleTime: 1 * MINUTE,
     cacheTime: 10 * MINUTE,
     retry: 1,
     suspense: true,
-    useErrorBoundary: true
+    useErrorBoundary: true,
+    ...options
   });
 
 export default useFetchGallery;
